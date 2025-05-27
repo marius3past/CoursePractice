759623c420a6179a466fd412125bc0a218ca20a5
marius3past <cibotarimarius@gmail.com>
2025-05-27 19:10:26 +0300

    Define the questions and answers

--- WhoWantsToBeAMillionaire/src/Main.java
+++ WhoWantsToBeAMillionaire/src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+           String QUESTION_1_LEVEL_1 = "Care planetă este cea mai apropiată de Soare?";
+        String QUESTION_1_ANSWER_1 = "Mercur";
+        String QUESTION_1_ANSWER_2 = "Marte";
+        String QUESTION_1_ANSWER_3 = "Venus";
+        String QUESTION_1_ANSWER_4 = "Saturn";
+        int QUESTION_1_MONEY = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String QUESTION_2_LEVEL_2 = "Care este cel mai lung fluviu din Europa?";
+        String QUESTION_2_ANSWER_1 = "Dunărea";
+        String QUESTION_2_ANSWER_2 = "Tamisa";
+        String QUESTION_2_ANSWER_3 = "Volga";
+        String QUESTION_2_ANSWER_4 = "Rinul";
+        int QUESTION_2_MONEY = 200;
+
+        String QUESTION_3_LEVEL_3 = "Ce companie a creat sistemul de operare Android?";
+        String QUESTION_3_ANSWER_1 = "Google";
+        String QUESTION_3_ANSWER_2 = "Samsung";
+        String QUESTION_3_ANSWER_3 = "Apple";
+        String QUESTION_3_ANSWER_4 = "Microsoft";
+        int QUESTION_3_MONEY = 500;
+
+
+        String QUESTION_4_LEVEL_3 = "În ce an a căzut Zidul Berlinului?";
+        String QUESTION_4_ANSWER_1 = "1985";
+        String QUESTION_4_ANSWER_2 = "1989";
+        String QUESTION_4_ANSWER_3 = "1991";
+        String QUESTION_4_ANSWER_4 = "1975";
+        int QUESTION_4_MONEY = 1000;
+
+
+
+        String QUESTION_5_LEVEL_5 = "Care este cel mai greu metal?";
+        String QUESTION_5_ANSWER_1 = "Aur";
+        String QUESTION_5_ANSWER_2 = "Plumb";
+        String QUESTION_5_ANSWER_3 = "Platina";
+        String QUESTION_5_ANSWER_4 = "Osmiu";
+        int QUESTION_5_MONEY = 2000;
     }
 }
